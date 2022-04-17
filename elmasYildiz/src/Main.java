b93529ce441362470582ea0c57be7d093e4c42c7
şevval balçık <97787298+sevvalbalcik@users.noreply.github.com>
2022-04-17 16:30:35 +0300

    first commit
    Kodluyoruz Back-End web development Bootcamp hazırlık eğitimi kapsamında yazdığım döngüler kullanarak yıldızlar ile elmas yazdırdığım java kodu.

---
+++ elmasYildiz/src/Main.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner inp = new Scanner(System.in);
+        int n;
+
+        System.out.print("N sayisini giriniz : ");
+        n = inp.nextInt();
+
+        for(int i=0;i<=n;i++)
+        {
+            for(int j=0;j<(n-i);j++)
+            {
+                System.out.print(" ");
+            }
+
+            for (int k=0;k<(2*i)-1;k++)
+            {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        for(int i=(n-1);i>=0;i--)
+        {
+            for(int j=0;j<(n-i);j++)
+            {
+                System.out.print(" ");
+            }
+
+            for (int k=0;k<(2*i)-1;k++)
+            {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+
+    }
+}
